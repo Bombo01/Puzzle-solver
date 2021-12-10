eaba93f7f2cb13c58b28a725f47e73c7f001cffb
lucab <lucabombonati31@gmail.com>
2021-12-10 08:20:53 +0100

    Sì

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,12 @@
+import java.util.*;
+
+/**
+ * Classe che rappresenta la tabella del 8 puzzle
+ *
+ * @author Luca Bombonati 2000247
+ */
 public class Board {
-    private final int[][] _board;
+    public final int[][] _board;
 
     /**
      * Costruttore della tabella
@@ -7,64 +14,10 @@ public class Board {
      * @param tiles l'array bidimensionale rappresentante la tabella
      */
     public Board(int[][] tiles) {
-        if (tiles.length == tiles[1].length) {
+        if (tiles.length == tiles[1].length)
             _board = tiles.clone();
-        } else
+        else
             throw new IllegalArgumentException();
-    }
-
-    /**
-     * Classe privata che descrive un nodo.
-     *
-     * Il nodo è, in questo caso, una tabella che deriva dallo
-     * spostamento dello zero dalla tabella genitore
-     */
-    private static class node {
-        public int level = 0; //livello del nodo
-        int[][] nodeBoard = new int[4][4];
-        node parent = null;
-
-        public node(int _level, int[][] board, node _parent) {
-            int level = _level;
-            nodeBoard = board.clone();
-            parent = _parent;
-            node up = move(1, 0);
-            node right = move(0, 1);
-            node down = move(-1, 0);
-            node left = move(0, -1);
-        }
-
-        /**
-         * Funzione che sposta lo zero in base a due parametri
-         * x ed y che possono assumere solo valori 0, 1 o -1
-         * @param x spostamento dello 0 nell'asse x (0 non sposta, 1 abbassa, -1 alza)
-         * @param y spostamento dello 0 nell'asse y (0 non sposta, 1 sposta a destra, -1 sposta a sinistra)
-         * @return il nodo con lo zero spostato
-         */
-        public node move(int x, int y){
-            int[][] _tempBoard = nodeBoard.clone();
-
-            int[] pos0 = Board.linearSearch(0, _tempBoard);
-            if((x > 0 && pos0[0] != 3) || (x < 0 && pos0[0] != 0)){
-                int temp = _tempBoard[pos0[0]+x][pos0[1]];
-                _tempBoard[pos0[0]][pos0[1]] = temp;
-                _tempBoard[pos0[0]+x][pos0[1]] = 0;
-
-                //verificare distanza mah
-
-                return new node(level+1, _tempBoard, ); //<-- trovare modo per dire del parentNode
-            } else if((y > 0 && pos0[1] != 3) || (y < 0 && pos0[1] != 0)){
-                int temp = _tempBoard[pos0[0]][pos0[1]+y];
-                _tempBoard[pos0[0]][pos0[1]] = temp;
-                _tempBoard[pos0[0]][pos0[1]+y] = 0;
-
-                //verificare distanza mah
-
-                return new node(_tempBoard);
-            } else{
-                throw new IllegalArgumentException();
-            }
-        }
     }
 
     /**
@@ -77,8 +30,9 @@ public class Board {
         int _manhattan = 0;
         int maxNumber = (_board.length * _board.length) - 1;
         for (int i = 1; i <= maxNumber; i++) {
-            int verticalDistance = Math.abs(linearSearch(i, _board)[0] - ((i - 1) / 4));  //calcolo distanza righe
-            int horizontalDistance = Math.abs(linearSearch(i, _board)[1] - ((i - 1) % 4));  //calcolo distanza colonne
+            int[] numberPos = linearSearch(i, _board);
+            int verticalDistance = Math.abs(numberPos[0] - ((i - 1) / _board.length));  //calcolo distanza verticale
+            int horizontalDistance = Math.abs(numberPos[1] - ((i - 1) % _board.length));  //calcolo distanza colonne
             _manhattan += verticalDistance + horizontalDistance;    //calcolo distanza totale
         }
         return _manhattan;
@@ -95,10 +49,95 @@ public class Board {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 if (array[i][j] == n)
-                    return new int[]{i, j};
+                    return new int[]{i, j}; //return un array come [riga][colonna]
             }
         }
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException(); //numero nella board non trovato
+    }
+
+    /**
+     * Funzione per clonare un array board
+     *
+     * @param array l'array da copiare
+     * @return l'array copiato
+     */
+    private int[][] copy2d(int[][] array) {
+        int[][] _temp = new int[_board.length][_board.length];
+        for (int i = 0; i < array.length; i++)
+            System.arraycopy(array[i], 0, _temp[i], 0, array[1].length); //copio ogni linea nella nuova matrice
+        return _temp;
+    }
+
+    /**
+     * Funzione per spostare la casella 0 lungo
+     * l'asse verticale e orizzontale
+     *
+     * @param vertical   > 0 per spostare lo zero verso destra
+     *                   < 0 per spostare lo zero verso sinistra
+     * @param horizontal > 0 per spostare lo zero verso il basso
+     *                   < 0 per spostare lo zero verso l'alto
+     * @return una nuova Board con lo zero traslato
+     */
+    public Board move(int vertical, int horizontal) {
+        int[][] _tempBoard = copy2d(_board);
+        int[] pos0 = Board.linearSearch(0, _tempBoard); //cerco lo zero nella tabella
+        if ((vertical > 0 && pos0[0] != _board.length - 1) || (vertical < 0 && pos0[0] != 0)) { //controllo se posso spostare lo zero
+            //sposto lo zero in verticale
+            int temp = _tempBoard[pos0[0] + vertical][pos0[1]];
+            _tempBoard[pos0[0]][pos0[1]] = temp;
+            _tempBoard[pos0[0] + vertical][pos0[1]] = 0;
+        } else if ((horizontal > 0 && pos0[1] != _board.length - 1) || (horizontal < 0 && pos0[1] != 0)) { //controllo se posso spostare lo zero
+            //sposto lo zero in orizzontale
+            int temp = _tempBoard[pos0[0]][pos0[1] + horizontal];
+            _tempBoard[pos0[0]][pos0[1]] = temp;
+            _tempBoard[pos0[0]][pos0[1] + horizontal] = 0;
+        }
+        return new Board(_tempBoard); //nuova tabella con lo zero spostato
+    }
+
+    /**
+     * Funzione che applica l'algoritmo A* con la distanza
+     * di manhattan per risolvere la Board
+     *
+     * @return il nodo rappresentante la board, il nodo successivo e il livello
+     */
+    public Node solver() {
+        PriorityQueue<Node> unexpandedNodes = new PriorityQueue<>();    //priority queue per salvare i nodi trovati ma non espansi
+        PriorityQueue<Node> expandedNodes = new PriorityQueue<>();  //priority queue per ricordarmi i nodi espansi
+
+        unexpandedNodes.add(new Node(this, null, 0/*, null*/)); //aggiungo la tabella base
+
+        while (true) {
+            Node minimumPriorityNode = unexpandedNodes.poll();  //tolgo il nodo con priorità minore
+            expandedNodes.add(minimumPriorityNode); //salvo il nodo appena tolto
+
+            Board minimumPriorityBoard = minimumPriorityNode.get_board();
+            int minimumPriorityLevel = minimumPriorityNode.get_level();
+
+            if (minimumPriorityBoard.manhattan() == 0)
+                return minimumPriorityNode; //se manhattan = 0 ho trovato la soluzione
+
+            //calcolo tutti i nodi
+            Node _up = new Node(minimumPriorityBoard.move(-1, 0), minimumPriorityNode, minimumPriorityLevel + 1);
+            Node _right = new Node(minimumPriorityBoard.move(0, 1), minimumPriorityNode, minimumPriorityLevel + 1);
+            Node _down = new Node(minimumPriorityBoard.move(1, 0), minimumPriorityNode, minimumPriorityLevel + 1);
+            Node _left = new Node(minimumPriorityBoard.move(0, -1), minimumPriorityNode, minimumPriorityLevel + 1);
+
+            /*
+            se trovo un nodo che ho già espanso (ad esempio
+            spostando lo zero in basso e subito dopo verso l'alto)
+            non ha senso riaggiungerlo perchè significa che seguendo
+            quella strada non troverei niente
+            */
+            if (!expandedNodes.contains(_up))
+                unexpandedNodes.add(_up);
+            if (!expandedNodes.contains(_right))
+                unexpandedNodes.add(_right);
+            if (!expandedNodes.contains(_down))
+                unexpandedNodes.add(_down);
+            if (!expandedNodes.contains(_left))
+                unexpandedNodes.add(_left);
+        }
     }
 
     /**
@@ -106,14 +145,105 @@ public class Board {
      *
      * @return la descrizione della tabella
      */
-
     public String toString() {
         StringBuilder _return = new StringBuilder();
-        for (int i = 0; i < _board.length; i++) {
-            for (int j = 0; j < _board[1].length; j++) {
-                _return.append(_board[i][j]).append(" ");
-            }
+        for (int[] ints : _board) {
+            for (int j = 0; j < _board[1].length; j++)
+                _return.append(ints[j]).append(" "); //aggiungo ogni valore della matrice alla stringa
         }
         return _return.toString();
+    }
+}
+
+/**
+ * Classe che rappresenta un nodo
+ *
+ * @author Luca Bombonati 2000247
+ */
+class Node implements Comparable {
+    private final Node _prev;
+    private final int _level;
+    private final Board _board;
+
+    /**
+     * Costruttore della classe nodo
+     *
+     * @param board la Board del nodo
+     * @param prev  il nodo precedente
+     * @param level l'altezza del nodo
+     */
+    public Node(Board board, Node prev, int level) {
+        _prev = prev;
+        _level = level;
+        _board = board;
+    }
+
+    /**
+     * Funzione che ritorna il nodo precendente
+     *
+     * @return il nodo precedente
+     */
+    public Node get_prev() {
+        return _prev;
+    }
+
+    /**
+     * Funzione che ritorna l'altezza del nodo
+     *
+     * @return l'altezza del nodo
+     */
+    public int get_level() {
+        return _level;
+    }
+
+    /**
+     * Funzione che ritorna la Board associata al nodo
+     *
+     * @return la Board associata
+     */
+    public Board get_board() {
+        return _board;
+    }
+
+    /**
+     * Funzione che compara 2 nodi e li mette in
+     * ordine per la distanza di manhattan e l'altezza
+     * del nodo
+     *
+     * @param o un altro nodo
+     * @return -1 se il nodo "o" viene prima del nodo chiamante questa funzione
+     * 0 se i due nodi sono uguali
+     * 1 se il nodo "o" viene dopo  1000000
+     */
+    @Override
+    public int compareTo(Object o) {
+        Node otherNode = (Node) o;
+        int thisNodePriority = this._level + _board.manhattan(); //calcolo la priorità del nodo
+        int otherNodePriority = otherNode._level + otherNode.get_board().manhattan(); //calcolo la priorità dell'altro nodo
+        return Integer.compare(thisNodePriority, otherNodePriority);
+    }
+
+    /**
+     * Funzione che compara due tabelle dei nodi
+     * e verifica se queste sono uguali. Viene utilizzato
+     * quando viene chiamato il priorityqueue.contains(Object o)
+     *
+     * @param o un altro nodo
+     * @return true o false se i due nodi hanno la board uguale
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Node node = (Node) o;
+
+        for (int i = 0; i < _board._board.length; i++) {
+            for (int j = 0; j < _board._board[i].length; j++) {
+                if(_board._board[i][j] != node._board._board[i][j])
+                    return false;
+            }
+        }
+        return true;
     }
 }
---
+++ src/Solver.java
@@ -0,0 +1,63 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+/**
+ * Programma che, una volta aver ricevuto i dati in
+ * ingresso da un file e averli organizzati sotto
+ * forma di una matrice 2d 3x3, crea la Board ed
+ * esegue il metodo "solver()" in modo da applicare
+ * l'algoritmo A* per trovare la soluzione
+ *
+ * @author Luca Bombonati 2000247
+ */
+public class Solver {
+    public static void main(String[] args) {
+        /* controllo se il numero di parametri passati dalla
+        riga di comandi è giusto o no */
+        if (args.length != 1) {
+            System.err.println("Uso: java Solver document.txt");
+        } else {
+            try {
+                int[][] boardArray = new int[3][3];
+                FileReader fileReader = new FileReader(args[0]);
+                Scanner scanner = new Scanner(fileReader);
+                while (scanner.hasNext()) { //finchè nel file sono presenti numeri li inserisco in boardArray
+                    for (int i = 0; i < boardArray.length; i++) {
+                        for (int j = 0; j < boardArray.length; j++) {
+                            boardArray[i][j] = scanner.nextInt();
+                        }
+                    }
+                }
+
+                Board b = new Board(boardArray); //creo la tabella
+                System.out.println(b.manhattan()); //stampo la distanza di manhattan
+
+                /*
+                ciò che ottengo è il percorso inverso, ma voglio visualizzare
+                le mosse in ordine quindi mi salvo il percorso e lo stampo al contrario
+                 */
+                ArrayList<String> reversePath = new ArrayList<>();
+
+                Node lastNode = b.solver(); //chiama l'algoritmo risolutivo
+
+                while (lastNode != null) { //salvo tutte le mosse nell'arraylist
+                    reversePath.add(lastNode.get_board().toString());
+                    lastNode = lastNode.get_prev();
+                }
+
+                //stampo l'arraylist al contrario
+                for (int i = reversePath.size() - 1; i >= 0; i--)
+                    System.out.println(reversePath.get(i));
+            } catch (IOException | NoSuchElementException | IllegalArgumentException ex) {
+                /*
+                errore sulla lettura del file oppure sui dati inseriti
+                 */
+                System.err.println("Errore! Controllare i dati inseriti");
+            }
+
+        }
+    }
+}
